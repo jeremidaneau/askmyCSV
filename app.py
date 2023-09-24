40ad24ca35cf48a64aaeac1323df9c68c99c2a01
jeremidaneau <jeremidaneau@gmail.com>
2023-09-24 16:27:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
-from textblob import TextBlob
 
 # Page title
 st.set_page_config(page_title='Ask the 📄 ')
